bcdb9566fcf235dfdca04d7b3d50eed1402a97f5
abbiehuynh <abbiehuynhh@gmail.com>
2024-07-25 01:57:02 -0500

    adds checkboxes to ingredients list and removes bullet points

--- recipe-page/index.js
+++ recipe-page/index.js
@@ -23,3 +23,42 @@ function clickMe() {
 function clickMeTwo() {
     document.getElementById("cohort").innerHTML = "We're going to get through this together!";
 }
+
+// changes background color of page when page loads
+function changeBackgroundColor(color) {
+    document.body.style.background = color;
+    document.getElementById("img").style.background = color;
+}
+window.addEventListener("load",function(){ changeBackgroundColor("#b3d9ae")});
+
+// b3d9ae
+
+// changes margin 
+function changeMargin(margin) {
+    document.getElementById("recipeHeader").style.margin = margin;
+}
+window.addEventListener("load", function(){ changeMargin("10px 400px") });
+
+
+// change padding of elements
+document.getElementById("recipeHeader").style.padding = "10px 10px 10px 10px";
+
+// creates rounded border radius 
+function roundCorners(round) {
+    document.getElementById("recipeHeader").style.borderRadius = round;
+    // come back to this, how to do class name?
+    // document.querySelectorAll("description").style.borderRadius = round;
+}
+window.addEventListener("load", function(){ roundCorners("25px") });
+
+// checkboxes
+let ingredientsList = document.getElementById("ingredientsList");
+// select the list and modify each individual list item
+// for each item, keep innertext, but turn content into an input checkbox
+
+let listItems = Array.from(ingredientsList.getElementsByTagName("li"));
+
+listItems.forEach(item => {
+  let text = item.innerText;
+  item.innerHTML = `<input type='checkbox' value='${text}'> ${text}</input>`;
+});
