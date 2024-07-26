efe040acacd7aabdf854c0099ec3acbf77bd5441
abbiehuynh <abbiehuynhh@gmail.com>
2024-07-26 01:42:45 -0500

    adds strikethrough when checkbox is clicked

--- recipe-page/index.js
+++ recipe-page/index.js
@@ -1,28 +1,22 @@
-
-// Adds statment after congratulations!
-var enjoy = document.getElementById("congratulations");
-var enjoyText = document.createTextNode(" Enjoy your yummy baked mac and cheese!");
+// Adds statement after "congratulations...
+let enjoy = document.getElementById("congratulations");
+let enjoyText = document.createTextNode(" Enjoy your yummy baked mac and cheese!");
 enjoy.appendChild(enjoyText);
 
-// function style() {
-// const ingredientsHeader = document.getElementsById("ingredientsHeader");
-//     ingredientsHeader.style.color = "red";
-// }
-
+/* for funsies
 // button: changes background color
 function backgroundColor(){
     document.body.style.backgroundColor = "white";
 }
-
 // button "cool"
 function clickMe() {
     document.getElementById("cool").innerHTML = "You're so cool!";
 }
-
 // button "cohort"
 function clickMeTwo() {
     document.getElementById("cohort").innerHTML = "We're going to get through this together!";
 }
+*/ 
 
 // changes background color of page when page loads
 function changeBackgroundColor(color) {
@@ -31,34 +25,75 @@ function changeBackgroundColor(color) {
 }
 window.addEventListener("load",function(){ changeBackgroundColor("#82b27c")});
 
-// b3d9ae
-
-// changes margin 
+// changes margin or recipeHeader
 function changeMargin(margin) {
     document.getElementById("recipeHeader").style.margin = margin;
 }
 window.addEventListener("load", function(){ changeMargin("70px 10px 10px") });
 
-
 // change padding of elements
-document.getElementById("recipeHeader").style.padding = "10px";
+// document.getElementById("recipeHeader").style.padding = "10px";
 
 // creates rounded border radius 
-function roundCorners(round) {
-    document.getElementById("recipeHeader").style.borderRadius = round;
-    // come back to this, how to do class name?
-    // document.querySelectorAll("description").style.borderRadius = round;
-}
-window.addEventListener("load", function(){ roundCorners("25px") });
+// function roundCorners(round) {
+//     document.getElementById("recipeHeader").style.borderRadius = round;
+//     // come back to this
+//     // document.querySelectorAll("description").style.borderRadius = round;
+// }
+// window.addEventListener("load", function(){ roundCorners("25px") });
+// label - ingredient, label text innerHTML
 
-// checkboxes
+// adds checkboxes to ingredients list
 let ingredientsList = document.getElementById("ingredientsList");
-// select the list and modify each individual list item
-// for each item, keep innertext, but turn content into an input checkbox
-
 let listItems = Array.from(ingredientsList.getElementsByTagName("li"));
 
 listItems.forEach(item => {
   let text = item.innerText;
-  item.innerHTML = `<input type='checkbox' value='${text}'> ${text}</input>`;
+  item.innerHTML = `<input type='checkbox' class= "checkbox" onclick="strikeThrough(this)" value='${text}'> ${text}</input>`;
 });
+
+// adds strikethrough to checkboxes when clicked
+let checkbox = document.getElementsByClassName("checkbox");
+
+// this is striking through all item, how to say just stike the item at 
+// the same position? [i]
+function strikeThrough(checkbox) {
+    if (checkbox.checked) {
+        ingredientsList.style.textDecoration = "line-through";
+    } else {
+        ingredientsList.style.textDecoration = "none";
+    }   
+};
+
+
+// please ignore below
+// checkbox.addEventListener("change", function() {
+//     listItems.style.textDecoration = checkbox.checked ? "line-through" : "none";
+// });
+
+// for (let i = 0; i < checkbox.length; i++) {
+//     checkbox[i].addEventListener("click", strikeThrough);
+// }
+
+// window.addEventListener("onclick", function(){}
+// )
+
+// function check() {
+//     if (this.checked) {
+//         checkbox.style.textDecoration = "line-through";
+//     } else {
+//         checkbox.style.textDecoration = "none";
+//     }
+// };
+// checkbox.addEventListener("change",
+// checkbox.type = "checkbox";
+// checkbox.name = "name";
+// checkbox.value = "value";
+// checkbox.id = "id";
+
+// let label = document.createElement("label");
+// label.htmlFor = "id";
+// label.appendChild(document.createTextNode("label"));
+
+// ingredientsList.appendchild(checkbox);
+// ingredientsList.appendChild(label);
