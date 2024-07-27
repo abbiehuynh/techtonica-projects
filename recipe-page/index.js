1ecb9e707013a27182245119bd00e5e36232147b
abbiehuynh <abbiehuynhh@gmail.com>
2024-07-27 18:45:35 -0500

    adds strikethrough feature to ingredients list

--- recipe-page/index.js
+++ recipe-page/index.js
@@ -1,36 +1,4 @@
-// Adds statement after "congratulations...
-let enjoy = document.getElementById("congratulations");
-let enjoyText = document.createTextNode(" Enjoy your yummy baked mac and cheese!");
-enjoy.appendChild(enjoyText);
 
-/* for funsies
-// button: changes background color
-function backgroundColor(){
-    document.body.style.backgroundColor = "white";
-}
-// button "cool"
-function clickMe() {
-    document.getElementById("cool").innerHTML = "You're so cool!";
-}
-// button "cohort"
-function clickMeTwo() {
-    document.getElementById("cohort").innerHTML = "We're going to get through this together!";
-}
-*/ 
-
-// ---WHEN PAGE LOADS---
-// changes background color of page when page loads
-function changeBackgroundColor(color) {
-    document.body.style.background = color;
-    document.getElementById("img").style.background = color;
-}
-window.addEventListener("load",function(){ changeBackgroundColor("#82b27c")});
-
-// changes margin or recipeHeader
-function changeMargin(margin) {
-    document.getElementById("recipeHeader").style.margin = margin;
-}
-window.addEventListener("load", function(){ changeMargin("70px 10px 10px") });
 
 // ---INGREDIENTS---
 // Ingredients List
@@ -62,59 +30,31 @@ ingredientsList.forEach(item => {
     let label = document.createElement("label");
     label.htmlFor = item;
 
+    // appending 
     label.appendChild(document.createTextNode(item));
     list.appendChild(checkbox);
     list.appendChild(label);
     ingredientsListDiv.appendChild(list);
 
     // styling 
+    checkbox.style.marginRight = "20px";
+
     list.style.fontFamily = "'Poppins', sans-serif";
-    list.style.fontSize = "1.5em";
+    list.style.fontSize = "1.4em";
     
     ingredientsListDiv.style.margin = "20px";
     ingredientsListDiv.style.padding = "30px 25px 10px";
     ingredientsListDiv.style.borderRadius = "40px";
     ingredientsListDiv.style.backgroundColor = "#b3d9ae";
 
+    // strikethrough ingredient when checkbox is clicked
+    checkbox.addEventListener("click", strikeThrough);
+    function strikeThrough() {
+        if (this.checked) {
+            label.style.textDecoration = "line-through";
+        } else {
+            label.style.textDecoration = "none";
+        }
+    };
+
 });
-
-// ---STRIKETHROUGH TEXT---
-// create strikeThrough when checkbox is clicked
-// function strikeThrough()) {
-//     if (checkbox.checked) {
-//         ingredientsList.style.textDecoration = "line-through";
-//     } else {
-//         ingredientsList.style.textDecoration = "none";
-//     }   
-// };
-
-// let item = event.currentTarget;
-//     item.style.textDecoration = "line-through";
-       
-
-// ingredientsList.style.textDecoration = "line-through";
-// } else {
-//     ingredientsList.style.textDecoration = "none";
-// }   
-// console.log(strikeThrough(e));
-
-// please ignore below
-// checkbox.addEventListener("change", function() {
-//     listItems.style.textDecoration = checkbox.checked ? "line-through" : "none";
-// });
-
-// for (let i = 0; i < checkbox.length; i++) {
-//     checkbox[i].addEventListener("click", strikeThrough);
-// }
-
-// window.addEventListener("onclick", function(){}
-// )
-
-// function check() {
-//     if (this.checked) {
-//         checkbox.style.textDecoration = "line-through";
-//     } else {
-//         checkbox.style.textDecoration = "none";
-//     }
-// };
-// checkbox.addEventListener("change",
