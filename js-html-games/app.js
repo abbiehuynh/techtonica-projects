bc400de3b7d85bcd22bf3ceb4c2b6a389df4a0eb
abbiehuynh <abbiehuynhh@gmail.com>
2024-08-08 00:08:11 -0500

    creates variable to access elements in html; adds some psudeocode

---
+++ js-html-games/app.js
@@ -0,0 +1,32 @@
+
+
+// creates variables to access elements in html
+const cat = document.querySelector(".cat");
+const grid = document.querySelector(".grid");
+const alert = document.getElementById("alert");
+
+// create an event listener for when DOM is loaded, starts game
+
+
+// control function
+    // creates connection to space bar to jump
+
+    // prevent user from double jumping while jumping
+
+
+// jump function 
+    // ability to move up
+    // ability to move back down, create jump height limit
+    // override css position
+
+// obstacle generator funciton
+    // while game is not game over, generate obstacles
+    // randomize generating
+    // override css position 
+
+    // removing obstacles when game is over
+
+
+// alerting when game is over
+
+
