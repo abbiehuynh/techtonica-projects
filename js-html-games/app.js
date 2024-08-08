37635498016b98337968ae9d90a5041e89bcfc4f
abbiehuynh <abbiehuynhh@gmail.com>
2024-08-08 13:53:32 -0500

    adds event listener for when DOM is loaded, start game

--- js-html-games/app.js
+++ js-html-games/app.js
@@ -1,32 +1,35 @@
-
-
-// creates variables to access elements in html
-const cat = document.querySelector(".cat");
-const grid = document.querySelector(".grid");
-const alert = document.getElementById("alert");
-
 // create an event listener for when DOM is loaded, starts game
+document.addEventListener("DOMContentLoaded", function() {
+    // creates variables to access elements in html
+    const cat = document.querySelector(".cat");
+    const grid = document.querySelector(".grid");
+    const alert = document.getElementById("alert");
 
 
-// control function
-    // creates connection to space bar to jump
-
-    // prevent user from double jumping while jumping
 
 
-// jump function 
-    // ability to move up
-    // ability to move back down, create jump height limit
-    // override css position
+    // control function
+        // creates connection to space bar to jump
 
-// obstacle generator funciton
-    // while game is not game over, generate obstacles
-    // randomize generating
-    // override css position 
-
-    // removing obstacles when game is over
+        // prevent user from double jumping while jumping
 
 
-// alerting when game is over
+    // jump function 
+        // ability to move up
+        // ability to move back down, create jump height limit
+        // override css position
 
+    // obstacle generator funciton
+        // while game is not game over, generate obstacles
+        // randomize generating
+        // override css position 
+
+        // removing obstacles when game is over
+
+
+    // alerting when game is over
+
+
+
+})
 
