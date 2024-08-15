fb2bff20ede1a192453ddf9547b058236022306f
abbiehuynh <abbiehuynhh@gmail.com>
2024-08-15 13:49:29 -0500

    creates useState hook, and creates state to track gameboard

--- react-game-connectfour/src/components/GameBoard.jsx
+++ react-game-connectfour/src/components/GameBoard.jsx
@@ -1,6 +1,21 @@
 import Slot from "./Slot";
+import { useState } from "react";
 
 const GameBoard = () => {
+
+    {/* creates state to track board */}
+    const GameBoard = () => {
+        const [board, setBoard] = useState([
+
+            {/* creates gameboard */}
+            ['','','','','','',''],
+            ['','','','','','',''],
+            ['','','','','','',''],
+            ['','','','','','',''],
+            ['','','','','','',''],
+            ['','','','','','',''],
+        ])
+    }
     return (
         <>
             {/* creates id for gameboard to access in css */}
