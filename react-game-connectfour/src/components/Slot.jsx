51b3815d781c87f0cca8f48a7f12239bce04756c
abbiehuynh <abbiehuynhh@gmail.com>
2024-08-15 13:45:25 -0500

    creates props to track player tokens and their positions on the game board

--- react-game-connectfour/src/components/Slot.jsx
+++ react-game-connectfour/src/components/Slot.jsx
@@ -1,10 +1,17 @@
 import greenToken from '../assests/greenToken';
 import pinkToken from '../assests/pinkToken';
 
-const Slot = () => {
+// create props to track player tokens (ch) and positions (y, x) inside of game board
+    // player tokens (X, O) - greenToken = X, pinkToken = O
+const Slot = ({ch, y, x}) => {
     return (
         <div className="slot">
-
+            {/* checks if slot is occupied, if ch is defined*/}
+            {ch && (
+                {/* if ch is X, show green token, else show pink token*/}
+                <img src={ch === "X" ? greenToken : pinkToken}
+                width="100%" height="100%" />
+            )}
         </div>
     )
 }
