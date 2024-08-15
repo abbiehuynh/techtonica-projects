8add2e03d11ff3a5deb0e203c46a8f788d3b1a80
abbiehuynh <abbiehuynhh@gmail.com>
2024-08-15 13:56:43 -0500

    creates states to track players and gameOver

--- react-game-connectfour/src/components/GameBoard.jsx
+++ react-game-connectfour/src/components/GameBoard.jsx
@@ -14,7 +14,19 @@ const GameBoard = () => {
             ['','','','','','',''],
             ['','','','','','',''],
             ['','','','','','',''],
-        ])
+        ]);
+
+        {/* creates states to track players */}
+        
+        {/* set initial state of current player to "X" = greenToken */}
+        const [currPlayer, setCurrPlayer] = useState("X");
+
+        {/* set initial state of opposing player to "O" = pinkToken */}
+        const [oppPlayer, setOppPlayer] = useState("O");
+
+        {/* create state for Game Over, boolean !isGameOver */ }
+        const [gameOver, setGameOver] = useState(false);
+
     }
     return (
         <>
