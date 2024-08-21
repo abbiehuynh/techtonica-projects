1c36385c9c1cce2e06677e5efda6283bcbf27849
abbiehuynh <abbiehuynhh@gmail.com>
2024-08-21 14:57:16 -0500

    adds array of 12 pokemon

---
+++ rest-api/pokemon.js
@@ -0,0 +1,77 @@
+const POKEMON = [
+{
+    "order": "#0001",
+    "name": "Bulbasaur",
+    "type": "Grass and Poison",
+    "evolution stage": "Basic"
+},
+{
+    "order": "#0002",
+    "name": "Ivysaur",
+    "type": "Grass and Poison",
+    "evolution stage": "Stage 1"
+},
+{
+    "order": "#0003",
+    "name": "Venusaur",
+    "type": "Grass and Poison",
+    "evolution stage": "Stage 2"
+},
+{
+    "order": "#0004",
+    "name": "Charmander",
+    "type": "Fire",
+    "evolution stage": "Basic"
+},
+{
+    "order": "#0005",
+    "name": "Charmeleon",
+    "type": "Fire",
+    "evolution stage": "Stage 1"
+},
+{
+    "order": "#0006",
+    "name": "Charizard",
+    "type": "Fire and Flying",
+    "evolution stage": "Stage 2"
+},
+{
+    "order": "#0007",
+    "name": "Squirtle",
+    "type": "Water",
+    "evolution stage": "Basic"
+},
+{
+    "order": "#0008",
+    "name": "Wartortle",
+    "type": "Water",
+    "evolution stage": "Stage 1"
+},
+{
+    "order": "#0009",
+    "name": "Blastoise",
+    "type": "Water",
+    "evolution stage": "Stage 2"
+},
+{
+    "order": "#0010",
+    "name": "Caterpie",
+    "type": "Bug",
+    "evolution stage": "Basic"
+},
+{
+    "order": "#0011",
+    "name": "Metapod",
+    "type": "Bug",
+    "evolution stage": "Stage 1"
+},
+
+{
+    "order": "#0012",
+    "name": "Butterfree",
+    "type": "Bug and Flying",
+    "evolution stage": "Stage 2"
+}
+]
+
+ export default POKEMON;
