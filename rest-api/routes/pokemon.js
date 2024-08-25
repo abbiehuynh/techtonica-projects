f27710e277a7962e50c671e2af1b2758c11b3ab6
abbiehuynh <abbiehuynhh@gmail.com>
2024-08-25 15:40:10 -0500

    adds uuid, and creates id for each pokemon

--- rest-api/routes/pokemon.js
+++ rest-api/routes/pokemon.js
@@ -1,81 +1,9 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const pokemon = [
-  { 
-    order: 1,
-    name: "Bulbasaur",
-    type: "Grass and Poison",
-    evolutionStage: "Basic"
-  }, 
-  {
-    order: 2,
-    name: "Ivysaur",
-    type: "Grass and Poison",
-    evolutionStage: "Stage 1"
-},
-{
-    order: 3,
-    name: "Venusaur",
-    type: "Grass and Poison",
-    evolutionStage: "Stage 2"
-},
-{
-    order: 4,
-    name: "Charmander",
-    type: "Fire",
-    evolutionStage: "Basic"
-},
-{
-    order: 5,
-    name: "Charmeleon",
-    type: "Fire",
-    evolutionStage: "Stage 1"
-},
-{
-    order: 6,
-    name: "Charizard",
-    type: "Fire and Flying",
-    evolutionStage: "Stage 2"
-},
-{
-    order: 7,
-    name: "Squirtle",
-    type: "Water",
-    evolutionStage: "Basic"
-},
-{
-    order: 8,
-    name: "Wartortle",
-    type: "Water",
-    evolutionStage: "Stage 1"
-},
-{
-    order: 9,
-    name: "Blastoise",
-    type: "Water",
-    evolutionStage: "Stage 2"
-},
-{
-    order: 10,
-    name: "Caterpie",
-    type: "Bug",
-    evolutionStage: "Basic"
-},
-{
-    order: 11,
-    name: "Metapod",
-    type: "Bug",
-    evolutionStage: "Stage 1"
-},
-{
-    order: 12,
-    name: "Butterfree",
-    type: "Bug and Flying",
-    evolutionStage: "Stage 2"
-}
-]
+const pokemon = [];
 
 // create get route to get all pokemon data
 router.get("/", (req, res) => {
@@ -89,15 +17,26 @@ router.post("/", (req, res) => {
     // console.log("POST ROUTE REACHED");
 
     // access and add pokemon data (no. 13) from postman to database
-    const pokemonInPostman = req.body;
+    const newPokemon = req.body;
 
-     // create an id for each pokemon
-     const pokemonId = uuidv4();
-     const pokemonWithId = { ...pokemon, id: pokemonId 
+    // create an id for each pokemon
+    const pokemonId = { ...newPokemon, id: uuidv4() }
+    pokemon.push(pokemonId);
 
-    pokemon.push(pokemonWithId);
-
-    res.send("pokemon with info");
+    res.send(`Pokemon with the order no. ${newPokemon.order} and name ${newPokemon.name} added to the database! Gotta Catch em' all!`);
 });
 
+// create delete route to delete pokemon data from database
+
+
+
+// create route 
+
 export default router;
+
+// {
+//     "order": 13,
+//     "name": "Weedle",
+//     "type": "Bug and Poison",
+//     "evolutionStage": "Basic"
+// }
