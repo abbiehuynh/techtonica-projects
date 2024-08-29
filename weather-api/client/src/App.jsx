7ac0c9e96a06e1295fc477b130df78bc393ed90d
abbiehuynh <abbiehuynhh@gmail.com>
2024-08-29 15:51:15 -0500

    adding useEffect and promise

--- weather-api/client/src/App.jsx
+++ weather-api/client/src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('')
+  useEffect(() => {
+    fetch('/')
+    .then((res) => res.text())
+    .then((data) => setMessage(data))
+    .catch((err) => console.log(err));
+  }, []);
 
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>{message}</h1>
+    </div>
   )
 }
 
-export default App
+export default App;
