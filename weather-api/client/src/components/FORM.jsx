e4d1a5e87653cf66b69a72d540ef657574b69838
abbiehuynh <abbiehuynhh@gmail.com>
2024-08-29 23:31:59 -0500

    adds psuedocode for states in components

--- weather-api/client/src/components/FORM.jsx
+++ weather-api/client/src/components/FORM.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
 
+// will initially appear on page
+// will include:
+    // text: "What the Weather is like in..."
+    // input (placeholder: enter city name)
+    // button (to submit form)
+
 const FORM = () => {
     const [submit, setSubmit] = useState('');
 }
--- weather-api/client/src/components/MESSAGE.jsx
+++ weather-api/client/src/components/MESSAGE.jsx
@@ -1,5 +1,13 @@
 import { useState } from 'react';
 
+// when the form is submitted, this "message" will appear
+// the message includes the following states: 
+    // city name
+    // current weather icon
+    // temperature
+    // humidity
+    // wind speed
+
 const MESSAGE = () => {
     const [message, setMessage] = useState('');
 }
