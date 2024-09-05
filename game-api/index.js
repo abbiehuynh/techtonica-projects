fbbdb5cb6a293714ffff5587bea310cd360d22d2
abbiehuynh <abbiehuynhh@gmail.com>
2024-09-05 00:23:43 -0500

    installing cors

--- game-api/index.js
+++ game-api/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 5050;
 
 app.use(bodyParser.json());
-
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
