c6c5cf27c9ffa5e2ac0bfef05a6dde13291e3516
abbiehuynh <abbiehuynhh@gmail.com>
2024-09-05 01:27:21 -0500

    creates form with label and multiple choices and submit button

--- game-api/client/src/components/game.jsx
+++ game-api/client/src/components/game.jsx
@@ -9,17 +9,17 @@ const GAME = () => {
 
 
     // fetches trivia api data from express server
-    const fetchTrivia = async () => {
-        try {
-            const response = await fetch(`http://localhost:5050/trivia-game`);
+    // const fetchTrivia = async () => {
+    //     try {
+    //         const response = await fetch(`http://localhost:5050/trivia-game`);
 
-            const data = await response.json();
-            setTriviaData(data);
+    //         const data = await response.json();
+    //         setTriviaData(data);
         
-        } catch (error) {
-            console.error("Error fetching trivia data:", error);
-        }
-    }
+    //     } catch (error) {
+    //         console.error("Error fetching trivia data:", error);
+    //     }
+    // }
 
     // creates useEffect to fetch data for default?
 
@@ -31,7 +31,31 @@ const GAME = () => {
 
   return (
     <div>
-        game here
+        <form class="form">
+            <label 
+                for="question" 
+                > question:
+                
+            </label>
+
+            <select
+                id="choices"
+                name="choices"
+                size="4"
+                multiple>
+                
+                <option value="choice 1">Choice 1</option>
+                <option value="choice 2">Choice 2</option>
+                <option value="choice 3">Choice 3</option>
+                <option value="choice 4">Choice 4</option>
+            </select>
+
+            <input type="submit"/>
+
+        
+
+
+        </form>
 
 
     </div>
