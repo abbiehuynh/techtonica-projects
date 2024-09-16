6280043baac9cb7e2a0a46720ce5fd4845ea737d
abbiehuynh <abbiehuynhh@gmail.com>
2024-09-16 13:47:38 -0500

    adds handleChange to search bar

--- eventonica/client/src/components/SearchBar.jsx
+++ eventonica/client/src/components/SearchBar.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SearchBar.css'
 
 const SearchBar = () => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  }
+
+  // if (searchInput.length > 0) {
+  //   eventname.filter((event) => {
+  //     return event.eventname.match(serachInput);
+  //   })
+  // }
+
+
   return (
     <div className="search-container">
       <p id="search" >Search For Event</p>
@@ -9,9 +23,8 @@ const SearchBar = () => {
         type="text"
         id="search-bar"
         placeholder="Search Here"
-        required
-        // value={}
-        // onChange={handleChange}
+        onChange={handleChange}
+        // value={searchInput}
       />
     </div>
   )
