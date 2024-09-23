aa4b15cf71f98fa8e7675487826d598685324797
abbiehuynh <abbiehuynhh@gmail.com>
2024-09-23 16:56:57 -0500

    creates super test to test get request for /species

--- animal-sighting-tracker/server/server.test.js
+++ animal-sighting-tracker/server/server.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('./server.js');
 
+// test get test connection
 describe('GET /test', () => {
     it('responds with JSON containing a message', async () => {
         const response = await request(app).get('/test');
@@ -8,4 +9,44 @@ describe('GET /test', () => {
         expect(response.status).toBe(200);
         expect(response.body).toEqual({ message: 'Hello, from ExpressJS with React-Vite' })
     });
+});
+
+// mock the database query to control the response of the db.query without hitting the actual database
+jest.mock('./db/db-connection.js', () => ({
+    query: jest.fn(),
+}));
+
+const db = require('./db/db-connection.js');
+
+// test get request for the endpoint /species
+describe('GET /species', () => {
+    it('responds with an array of species', async () => {
+        // mocks the database response
+        const mockSpecies = [
+            { 
+                id: 1, 
+                common_name: 'Bengal Tiger', 
+                scientific_name: 'panthera tigris tigris',
+                population_count: 2500, 
+                conservation_status_code: 'EN', 
+                record_creation_timestamp: '2021-07-10'
+            }
+        ];
+        db.query.mockResolvedValue({ rows: mockSpecies });
+
+        const response = await request(app).get('/species');
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(mockSpecies);
+    });
+
+    // it('responds with status 400 on error', async () => {
+    //     // mock the database to throw an error
+    //     db.query.mockRejectedValue(new Error('Database Error'));
+        
+    //     const response = await request(app).get('/species');
+
+    //     expect(response.status).toBe(400);
+    //     expect(response.body).toEqual({ error: 'Database error' });
+    // });
 });
