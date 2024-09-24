81949d77f8bbe3e732d3754212083fbbbb1374ad
abbiehuynh <abbiehuynhh@gmail.com>
2024-09-24 16:20:16 -0500

    installs packages for server

--- contact-list-app/server/server.js
+++ contact-list-app/server/server.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+
 require('dotenv').config();
 const path = require('path');
 const db = require('./db/db-connection.js');
 
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
 // creates an endpoint for the route "/""
 app.get('/', (req, res) => {
-    res.json({ message: 'Hola, from My template ExpressJS with React-Vite' });
+    res.json({ message: 'Hello, from ExpressJS with React-Vite' });
 });
 
 // create the get request for students in the endpoint '/api/students'
