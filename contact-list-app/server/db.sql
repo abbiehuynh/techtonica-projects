f464562becb02bfc545a89527b9601fd95ff25d8
abbiehuynh <abbiehuynhh@gmail.com>
2024-09-25 11:50:03 -0500

    Adds insert values for db dump tables

--- contact-list-app/server/db.sql
+++ contact-list-app/server/db.sql
@@ -28,10 +28,22 @@ CREATE TABLE public.contacts (
     id INT NOT NULL,
     firstname VARCHAR(255),
     lastname VARCHAR(255),
+    name VARCHAR(255),
     email VARCHAR(255), 
     phone_number VARCHAR(20),
     notes VARCHAR(255)
 );
+
+INSERT INTO public.contacts(id, name, email, phone_number, notes)
+VALUES 
+    (1, 'Finn', 'finn_the_human@gmail.com', '265-789-1234', 'The Human'),
+    (2, 'Jake', 'jake_the_dog@gmail.com', '265-456-7865', 'The Dog'),
+    (3, 'BMO', 'bmo@gmail.com', '256-154-1235', 'A Computer?'),
+    (4, 'Princess Bubblegum', 'the_princess-bubblegum@yahoo.com', '456-123-7894', 'A Princess'),
+    (5, 'Lumpy Space Princess', 'the_lumpy_space_princess@hotmail.com', '456-782-1258', 'A Princess'),
+    (6, 'Flame Princess', 'the_flame_princess@yahoo.com', '456-129-6587', 'A Princess'),
+    (7, 'Marceline', 'the_vampire_queen', '756-354-9587', 'The Vampire Queen')
+
 
 -- Name: personal_details; Type: TABLE; Schema: public; Owner: -
 --
@@ -45,6 +57,16 @@ CREATE TABLE public.personal_details (
     image_url VARCHAR(255)
 );
 
+INSERT INTO public.personal_details(id, location, notes)
+VALUES 
+    (1, 'Land of Ooo', 'Best Friends with Jake'),
+    (2, 'Land of Ooo', 'Best Friends with Finn'),
+    (3, 'Land of Ooo', 'Rescued by Finn and Jake from pirates in the Evil Forest'),
+    (4, 'Candy Kingdom', 'Ruler of the Candy Kingdom'),
+    (5, 'Evil Forest', 'Leader of the Pirates'),
+    (6, 'Fire Kingdom', 'Ruler of the Fire Kingdom'),
+    (7, 'Land of Ooo', 'Half-human and half-demon vampire')
+
 -- Name: work_details; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -55,6 +77,17 @@ CREATE TABLE public.work_details (
     location VARCHAR(255),
     link VARCHAR(255)
 );
+
+INSERT INTO public.work_details(id, occupation, location)
+VALUES 
+    (1, 'Hero Champion', 'Candy Kingdom'),
+    (2, 'Hero', 'Candy Kingdom'),
+    (3, 'Video Game Console', 'Jake and Finns House'),
+    (4, 'Scientist, Inventor, Ruler', 'Candy Kingdom'),
+    (5, 'Leader of Pirates', 'Evil Forest'),
+    (6, 'Ruler', 'Fire Kingdom'),
+    (7, 'Musician', 'Land of Ooo')
+
 
 --
 -- Name: contacts_id_seq; Type: SEQUENCE; Schema: public; Owner: -
