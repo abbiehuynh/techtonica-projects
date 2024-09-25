8dc7b45b93667ef5001c213f7f28baa73ed75026
abbiehuynh <abbiehuynhh@gmail.com>
2024-09-25 17:09:26 -0500

    saving progress working on put request

--- contact-list-app/server/server.js
+++ contact-list-app/server/server.js
@@ -139,25 +139,42 @@ app.delete('/contacts/details/:contactId', async (req, res) => {
     }
 });
 
-//A put request - Update a student 
-app.put('/api/students/:studentId', async (req, res) =>{
+// creates a put request to update a contact
+app.put('/contacts/details/:contactId', async (req, res) =>{
     //console.log(req.params);
     //This will be the id that I want to find in the DB - the student to be updated
-    const studentId = req.params.studentId
-    const updatedStudent = { id: req.body.id, firstname: req.body.firstname, lastname: req.body.lastname, iscurrent: req.body.is_current}
-    console.log("In the server from the url - the student id", studentId);
-    console.log("In the server, from the react - the student to be edited", updatedStudent);
-    // UPDATE students SET lastname = "something" WHERE id="16";
-    const query = `UPDATE students SET firstname=$1, lastname=$2, is_current=$3 WHERE id=${studentId} RETURNING *`;
-    const values = [updatedStudent.firstname, updatedStudent.lastname, updatedStudent.iscurrent];
+    const contactId = req.params.contactId
+    const updatedContact = { 
+        name: req.body.id, 
+        email: req.body.email,
+        phone_number: req.body.phone_number,
+        notes: req.body.notes,
+        location: req.body.location,
+        occupation: req.body.occupation
+    }
+
+    console.log("In the server from the url - the contact id", contactId);
+    console.log("In the server, from the react - the contact to be edited", updatedContact);
+    
+    const query = `UPDATE contacts SET name=$1, email=$2, phone_number=$3, notes=$4, occupation=$5 WHERE id=${contactId} RETURNING *`;
+    const values = [
+        contactsId,
+        updatedContact.name, 
+        updatedContact.email,
+        updatedContact.phone_number,
+        updatedContact.notes,
+        updatedContact.location,
+        updatedContact.occupation
+    ];
+
     try {
       const updated = await db.query(query, values);
       console.log(updated.rows[0]);
       res.send(updated.rows[0]);
   
-    }catch(e){
-      console.log(e);
-      return res.status(400).json({e})
+    }catch(error){
+      console.error("Error Uploading Updated Contact: ", error);
+      return res.status(400).json({error: 'An error occured while processing your put request.'});
     }
   })
 
