34b4b1a05f6226db4ad5f62609c3bd4a0e86b018
abbiehuynh <abbiehuynhh@gmail.com>
2024-09-25 12:01:49 -0500

    adds get requests to tables in db

--- contact-list-app/server/server.js
+++ contact-list-app/server/server.js
@@ -19,11 +19,33 @@ app.get('/', (req, res) => {
     res.json({ message: 'Hello, from ExpressJS with React-Vite' });
 });
 
-// create the get request for the contact list in the endpoint '/contact-list'
-app.get('/contact-list', async (req, res) => {
+// create the get request for the contact list in the endpoint '/contacts'
+app.get('/contacts', async (req, res) => {
     try {
-        const { rows: contacts } = await db.query('SELECT * FROM contacts');
+        const { rows: contacts } = await db.query('SELECT * FROM public.contacts');
         res.send(contacts);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ error });
+    }
+});
+
+// create get request for contact's personal details in the endpoint /contacts/personal-details
+app.get('/contacts/personal-details', async (req, res) => {
+    try {
+        const { rows: personal_details } = await db.query('SELECT * FROM public.personal_details');
+        res.send(personal_details);
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({ error });
+    }
+});
+
+// create get request for contact's work details in the endpoint /contacts/work-details
+app.get('/contacts/work-details', async (req, res) => {
+    try {
+        const { rows: work_details } = await db.query('SELECT * FROM public.work_details');
+        res.send(work_details);
     } catch (error) {
         console.log(error);
         return res.status(400).json({ error });
@@ -91,5 +113,5 @@ app.put('/api/students/:studentId', async (req, res) =>{
 
 // console.log that your server is up and running
 app.listen(PORT, () => {
-    console.log(`Hola, Server listening on ${PORT}`);
+    console.log(`Hello, Server listening on ${PORT}`);
 });
