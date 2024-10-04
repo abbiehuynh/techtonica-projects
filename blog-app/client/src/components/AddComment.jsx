6a7f0b184e504a88cc916659e2206da3370e3b6c
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-04 16:26:54 -0500

    creates form input to add comment

--- blog-app/client/src/components/AddComment.jsx
+++ blog-app/client/src/components/AddComment.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import { Button, Form } from "react-bootstrap"
 
 const AddComment = () => {
   return (
-    <div>AddComment</div>
+    <div style={{paddingTop:'20px'}}>
+        <Form className='form-comments' >
+            <Form.Group className='form-group'>
+                <h2 id="create-comment-header">Add a Comment</h2>
+                <Form.Label className='form-label' style={{marginLeft: '10px'}}>Name</Form.Label>
+                <input
+                    type="text"
+                    id="add-name"
+                    placeholder="Your Name"
+                    required
+                    // value={postDetails.comment_author}
+                    // onChange={handleCommentNameChange}
+                    style={{borderRadius: '20px', padding: '10px'}}
+                />
+            </Form.Group>
+            <Form.Group className='form-group'>
+                <Form.Label className='form-label' style={{marginLeft: '10px'}}>Comment</Form.Label>
+                <input
+                    type="text"
+                    id="add-comment"
+                    placeholder="Your Comment"
+                    required
+                    // value={postDetails.comment_content}
+                    // onChange={handleCommentChange}
+                    style={{borderRadius: '20px', padding: '10px'}}
+                />
+            </Form.Group>
+
+            <Form.Group>
+                {/* post request to comments db */}
+                <Button
+                    type="submit"
+                    >Add Comment
+                </Button>
+            </Form.Group>
+            
+            {/* <Form.Group>
+            <Button type="submit" variant="outline-success">{PostDetails.comment_id ? "Edit Post" : "Add Post"}</Button>
+            {PostDetails.comment_id ? <Button type="button" variant="outline-warning" onClick={clearForm}>Cancel</Button> : null}
+            </Form.Group> */}
+        </Form>
+
+    </div>
   )
 }
 
