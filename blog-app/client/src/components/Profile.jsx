346e41ad33a874a00e13068cd5779a209a1e7734
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-04 11:48:42 -0500

    adds styling and structure to profile page

--- blog-app/client/src/components/Profile.jsx
+++ blog-app/client/src/components/Profile.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    // to do: create users table and use data to show information here
+    <div id="profile-card">
+      <div id="profile-container1">
+        <img src='/src/assets/oliver-pfp.jpg' style={{height: '400px'}} id="pfp"></img>
+        <div id="bio">
+          <h1> Oliver </h1>
+          <h2> he/him </h2>
+          <p>status: sleeping, of course! </p>
+        </div>
+      </div>
+
+      <div id="profile-container2">
+        <div id="my-posts">
+          My Posts
+          <div >
+
+          </div>
+        </div>
+
+        <div id="my-comments">
+          My Comments
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
