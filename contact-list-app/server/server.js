40c03f185ff86db89efc935a3620560eed912b51
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-19 18:27:48 -0500

    Adds routes folder to hold test routes to database tables

--- contact-list-app/server/server.js
+++ contact-list-app/server/server.js
@@ -19,38 +19,9 @@ app.get('/', (req, res) => {
     res.json({ message: 'Hello, from ExpressJS with React-Vite' });
 });
 
-// create the get request for the contact list in the endpoint '/contacts'
-app.get('/contacts', async (req, res) => {
-    try {
-        const { rows: contacts } = await db.query('SELECT * FROM public.contacts');
-        res.send(contacts);
-    } catch (error) {
-        console.log(error);
-        return res.status(400).json({ error });
-    }
-});
-
-// create get request for contact's personal details in the endpoint /contacts/personal-details
-app.get('/contacts/personal-details', async (req, res) => {
-    try {
-        const { rows: personal_details } = await db.query('SELECT * FROM public.personal_details');
-        res.send(personal_details);
-    } catch (error) {
-        console.log(error);
-        return res.status(400).json({ error });
-    }
-});
-
-// create get request for contact's work details in the endpoint /contacts/work-details
-app.get('/contacts/work-details', async (req, res) => {
-    try {
-        const { rows: work_details } = await db.query('SELECT * FROM public.work_details');
-        res.send(work_details);
-    } catch (error) {
-        console.log(error);
-        return res.status(400).json({ error });
-    }
-});
+// test get request to tables in database
+const testTableRoutes = require('./routes/testTableRoutes.js');
+app.use('/testTable', testTableRoutes);
 
 // create get request for ALL three tables - contacts, personal_details, and work_details
 app.get('/contacts/details', async (req, res) => {
