df6a7ae09771197f10b14145db8e77240bb1352f
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-19 19:34:27 -0500

    adds home page and link to contacts from home page

--- contact-list-app/client/src/components/Home.jsx
+++ contact-list-app/client/src/components/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div style={{display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column",
+        margin: "100px"
+    }}>
+        <h1>Welcome to your Contact List!</h1>
+        <p>Go to
+            <Button>
+                <Link to="/contacts"> Contacts </Link>
+            </Button> 
+            to check your contact list.</p>
+
+    </div>
   )
 }
 
