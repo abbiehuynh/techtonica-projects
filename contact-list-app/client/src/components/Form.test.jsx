ff7f7bd38b2b617ae451aaacb7d1975ff4ed71e2
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-19 21:21:38 -0500

    have testing enviornment set up and running

--- contact-list-app/client/src/components/Form.test.jsx
+++ contact-list-app/client/src/components/Form.test.jsx
@@ -1,4 +1,21 @@
 import React from 'react';
+import '@testing-library/jest-dom';
 import { render, screen, fireEvent } from '@testing-library/react';
-import Form from './Form';
+import MyForm from './Form';
 
+// creates testing suite
+describe('MyForm', () => {
+    // creates mock of OnSaveContact
+    const mockOnSaveContact = jest.fn();
+
+    // this will run before each test in the suite - renders onSaveContact in Form Component
+    beforeEach(() => {
+        render(<MyForm onSaveContact={mockOnSaveContact} />);
+    });
+
+    test('renders form fields', () => {
+        expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument();
+        expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument();
+    })
+
+})
