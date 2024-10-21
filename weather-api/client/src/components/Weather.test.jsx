ee7c59723e419434c20e7b9ea85b9a9cd077ca56
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-20 22:47:21 -0500

    adds and passes second test for updating city input

--- weather-api/client/src/components/Weather.test.jsx
+++ weather-api/client/src/components/Weather.test.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Weather from './WEATHER';
 
 // mock setFavorite City function
 const mockSetFavoriteCity = jest.fn();
 
 describe('Weather Component', () => {
-    
-    test('renders weather forecast on webpage', () => {
+
+    // test 1
+    test('renders weather input and button on webpage', () => {
         render(<Weather userId={1} setFavoriteCity={mockSetFavoriteCity} />);
     
     // check if the form input is present
     const inputElement = screen.getByPlaceholderText(/Enter City Name/i);
     expect(inputElement).toBeInTheDocument();
-    })
+
+    // checks if the button is present
+    const buttonElement = screen.getByText(/Get Weather/i);
+    expect(buttonElement).toBeInTheDocument();
+    });
+
+    // test 2
+    test('updates city input', () => {
+        render(<Weather userId={1} setFavoriteCity={mockSetFavoriteCity} />);
+        
+        const inputElement = screen.getByPlaceholderText(/Enter City Name/i);
+        fireEvent.change(inputElement, { target: { value: 'London' } });
+        
+        expect(inputElement.value).toBe('London');
+      });
 })
