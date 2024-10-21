7b38e914f33f4159b2f31072ba79f1bd645136c8
abbiehuynh <abbiehuynhh@gmail.com>
2024-10-20 19:58:34 -0500

    Creates get request to check if city is user's favorite city by user id and city name, creates post request to add user and favorite city to database table, creates put request to update user's favorite city

--- weather-api/server/server.js
+++ weather-api/server/server.js
@@ -43,21 +43,82 @@ const getWeather = async (req, res) => {
 // creates get route to use getWeather function
 app.get('/weather', getWeather)
 
-// creates get request for data from users table for specific user
-app.get('/users/:user_id', async (req, res) => {
-    const userId = req.params.user_id;
+// creates get request for if city is user's favorite city by user id and city name
+app.get('/favorite-city/:userId/:city', async (req, res) => {
+    const { userId, city } = req.params;
 
     try {
-        const { rows: users } = await db.query(
-            `SELECT * FROM public.users WHERE id = $1;`, [userId]
+        const result = await db.query(
+            `SELECT EXISTS(SELECT 1 FROM users WHERE id = $1 AND favorite_city = $2);`, 
+            [userId, city]
         );
-        res.send(users);
+
+        const isFavorite = result.rows[0].exists;
+        res.json({ isFavorite });
     } catch (error) {
-        console.error("Error fetching projects data", error );
-        return res.status(400).json({ error });
+        console.error("Error checking favorite city:", error );
+        res.status(500).json({ erro: "Internal Server Error" });
     }
 });
 
+// create post request to add favorite city and user to database table
+app.post('/favorite-city', async (req, res) => {
+    const { userId, city } = req.body;
+
+    try {
+        const result = await db.query(
+            `INSERT INTO users(id, favorite_city) 
+            VALUES ($1, $2) 
+            RETURNING *;`, [userId, city]
+        );
+
+        res.status(201).json({ 
+            message: "City added to favorites.", 
+            city: result.rows[0] 
+        });
+
+    } catch (error) {
+        console.error("Error adding favoirte city:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+// creates put request to update favorite city of exisitng user 
+app.put('/favorite-city/:userId', async (req, res) => {
+    // gets userId from url params
+    const { userId } = req.params;
+    // expects city in the request body
+    const { favorite_city } = req.body;
+    console.log(req.body);
+
+    if (!favorite_city) {
+        return res.status(400).json({ error: "City is required." });
+    }
+
+    try {
+        // updates the user's favorite city in the database
+        const result = await db.query(
+          'UPDATE users SET favorite_city = $1 WHERE id = $2 RETURNING *',
+          [favorite_city, userId]
+        );
+    
+        if (result.rowCount === 0) {
+          return res.status(404).json({ error: "Favorite city not found for the user." });
+        }
+    
+        // Return success response
+        res.status(200).json({
+          message: "Favorite city updated.",
+          favoriteCity: result.rows[0], // Return the updated favorite city object
+        });
+
+    } catch (error) {
+        console.error("Error updating favorite city:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
-})
+});
